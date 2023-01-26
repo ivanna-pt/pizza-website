e1f9c68eb0ba6be56936dfa158f1725e2dc244d3
ivanna-pt <ivanna.pokhyl@gmail.com>
2023-01-26 23:25:26 +0200

    add clean func for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
+const del = require('del');
 
 const requireDiv = require('require-dir');
 const tasks = requireDiv('./tasks');
 
+function clean () {
+    return del(['dist'])
+}
+
 exports.hello = tasks.hello;
 exports.style = tasks.style;
-
+exports.clean = clean
