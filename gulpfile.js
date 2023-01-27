ffc0f90a6fc190cef4a9aea7d3661ffb2995198a
ivanna-pt <ivanna.pokhyl@gmail.com>
2023-01-27 11:16:51 +0200

    create styles gulp-task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 const gulp = require('gulp');
-const del = require('del');
 
-//for exports
+const del = require('del');
+const bs = require('browser-sync');
+
+const sass = require('gulp-sass')(require('sass'));
+const bulk = require('gulp-sass-bulk-importer');
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const sourcemap = require('gulp-sourcemaps');
+
+
+//for old exports
 const requireDiv = require('require-dir');
 const tasks = requireDiv('./tasks');
 
+
+//Paths
 const path = {
     styles: {
         src: 'src/styles/**/*.scss',
@@ -16,14 +28,41 @@ const path = {
     }
 }
 
+//Build Styles Task
+function styles () {
+    return gulp.src(path.styles.src)
+        .pipe(sourcemap.init())
+        .pipe(bulk())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 8 versions'],
+            browsers: [
+                'Android >= 4',
+                'Chrome >= 20',
+                'Firefox >= 24',
+                'Explorer >= 11',
+                'iOS >= 6',
+                'Opera >= 12',
+                'Safari >= 6',
+            ]
+        }))
+        .pipe(clean({ level: 2}))
+        .pipe(concat('style.min.css'))
+        .pipe(sourcemap.write('../sourcemap/'))
+        .pipe(gulp.dest(path.styles.dest))
+}
 
 function clean () {
-    return del(['dist'])
+    return del(['build'])
 }
 
 //old exports
-exports.hello = tasks.hello;
-exports.style = tasks.style;
+// exports.hello = tasks.hello;
+// exports.style = tasks.style;
 
 //new exports
 exports.clean = clean
+exports.styles = styles
+
